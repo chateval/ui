002c4f42f43e01098dc39f233437e6d018ab8a9b
Arun Kirubarajan <arun.kirubarajan@gmail.com>
2018-09-26 15:34:24 -0400

    Change header link to live site

--- components/Header.js
+++ components/Header.js
@@ -11,7 +11,7 @@ const Header = () => (
     </head>
 
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="/">ChatEval</a>
+      <a className="navbar-brand" href="https://chateval.org">ChatEval</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
