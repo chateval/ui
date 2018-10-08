4b2cbc381a60ebbdf2573ed060d7d90253bb0f78
Arun Kirubarajan <arun.kirubarajan@gmail.com>
2018-10-08 18:07:36 -0400

    Add descriptions to about page

--- components/Card.js
+++ components/Card.js
@@ -4,7 +4,7 @@ const Card = (props) => (
       <h5 class="card-title">{props.title}</h5>
       <h6 class="card-subtitle mb-2 text-muted">{props.subtitle}</h6>
       <p class="card-text">{props.description}</p>
-      <a href={props.url} class="card-link">{props.link}</a>
+      <a href={props.url} class="btn btn-sm btn-primary">{props.link}</a>
     </div>
   </div>
 );
--- pages/about.js
+++ pages/about.js
@@ -10,34 +10,40 @@ const About = (props) => (
       <h1 class="mt-5">About Evaluation</h1>
       <p class="lead">Model responses are generated using an evaluation dataset of prompts and then uploaded to ChatEval. The responses are then 
       evaluated using a series of automatic evaluation metrics, and are compared against selected baseline/ground truth models (e.g. humans). </p>
+
       <h2 class="mt-4">Evaluation Datasets</h2>
-      <br />
+      <p>ChatEval offers evaluation datasets consisting of prompts that uploaded chatbots are to respond to. Evaluation datasets are available
+        to download for free and have corresponding baseline models. </p>
       <div class="row">
         {props.evalsets.map(evalset => 
-          <div className="col-md-3">
-            <Card title={evalset.long_name} subtitle={"Supported ChatEval Dataset"} description={evalset.description} />
+          <div className="col-md-6">
+            <Card title={evalset.long_name} subtitle={"Supported ChatEval Dataset"} description={evalset.description} link={"Download Dataset"} url={evalset.source} />
             <br />
           </div>
         )}
       </div>
 
-      <h2 class="mt-4">ChatEval Baseline Models</h2>
-      <br />
-      <div class="row">
+      <h2 class="mt-4">ChatEval Baselines</h2>
+      <p>
+        ChatEval offers "ground-truth" baselines to compare uploaded models with. Baseline models range from human responders to established chatbot models.
+        Baselines are handpicked and uploaded by the ChatEval Team.
+      </p>
+      <div class="row"> 
         {props.baselines.map(baseline => 
-          <div className="col-md-3">
-            <Card title={baseline.name} url={"/model?id="+baseline.model_id} link={"View"} subtitle={baseline.description} />
+          <div className="col-md-4">
+            <Card title={baseline.name} url={"/model?id="+baseline.model_id} link={"View Model"} subtitle={baseline.description} />
             <br />
           </div>
         )}
       </div>
 
       <h2 class="mt-4">Automated Evaluation Methods</h2>
-      <br />
+      <p> The ChatEval Platform handles certain automated evaluations of chatbot responses. These metrics are documented <a href="https://github.com/chateval/evaluation">here</a>. 
+      Models can be ranked according to a specific metric and viewed as a leaderboard.</p>
       <div class="row">
         {props.metrics.map(metric => 
           <div className="col-md-3">
-            <Card title={metric.name} subtitle={"Metric"} description={metric.info} />
+            <Card title={metric.name} subtitle={"Metric"} description={metric.info} link={"View Source"} url={"https://github.com/chateval/evaluation"} />
             <br />
           </div>
         )}
