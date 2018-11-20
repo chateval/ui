894a7396e997d730926a7afa021370af74bf8fdd
jsedoc <jsedoc@users.noreply.github.com>
2018-11-20 11:01:25 -0500

    Update index.js
    I've added the video and made some small changes.

--- pages/index.js
+++ pages/index.js
@@ -8,14 +8,16 @@ const Index = () => (
       <h1 className="mt-5 font-weight-bold"> ChatEval</h1>
       <p className="lead">ChatEval is a scientific framework for evaluating <mark>open domain chatbots</mark>. 
       Researchers can submit their trained models to effortlessly receive comparisons with baselines and prior work. 
-      Since all evaluation code is <mark>open source</mark>, we ensure evaluation is performed in a <mark>standardized</mark> and transparent way. 
+      Since all evaluation code is <mark>open source</mark>, we ensure evaluation is performed in a standardized and transparent way. 
       Additionally, open source baseline models and an ever growing groups public evaluation sets are available for public use.</p>
       <a href="https://my.chateval.org/upload" className="btn btn-primary">Upload Model</a>
       <br /> <br />
       <h5 className="card-title"> How much does ChatEval cost? </h5>
-      <p className="card-text">ChatEval is <mark>free</mark> for developers. It is actively developed by researchers at the NLP Group of the University of Pennyslvania.</p>
-      <h5 className="card-title"> How is automatic chatbot evaluation performed? </h5>
-      <p className="card-text">Read more about how automatic evaluation is done <a href="/about">here</a>.</p>
+      <p className="card-text">ChatEval is currently <mark>free</mark> for academic researchers. It is actively developed by the NLP Group of the University of Pennyslvania.</p>
+      <h5 className="card-title"> Is there an online demo video? </h5>
+      <p className="card-text">You can find a video tutorial for ChatEval <a href="https://youtu.be/36rAoujxLAA">here</a>.</p>
+      <h5 className="card-title"> How is automatic chatbot model assesment and evaluation performed? </h5>
+      <p className="card-text">Read more about how automatic model assessment and evaluation is done <a href="/about">here</a>.</p>
       <h5 className="card-title"> How was ChatEval built? </h5>
       <p className="card-text">The ChatEval webapp is built using Django and React (front-end) using Magnitude word embeddings format for evaluation. Our source code is available on <a href="https://github.com/chateval">Github</a>.</p>
     </main>
