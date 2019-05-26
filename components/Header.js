39414560218f53077af9e48b14aca4bd4c6880e4
Daphne Ippolito <daphnei@seas.upenn.edu>
2019-05-26 16:51:23 -0700

    Switch to use of word "System"

--- components/Header.js
+++ components/Header.js
@@ -18,7 +18,7 @@ const Header = () => (
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item">
-            <a class="nav-link" href="/models">View Methods</a>
+            <a class="nav-link" href="/models">View Systems</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="/conversations">View Conversations</a>
