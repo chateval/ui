0906365cda0b447a100eac0914136ec7d5969d07
Daphne Ippolito <daphnei@seas.upenn.edu>
2019-06-01 10:22:19 -0700

    s/system/method/

--- components/Header.js
+++ components/Header.js
@@ -18,7 +18,7 @@ const Header = () => (
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item">
-            <a className="nav-link" href="/models">View Systems</a>
+            <a className="nav-link" href="/models">View Methods</a>
           </li>
           <li className="nav-item">
             <a className="nav-link" href="/conversations">View Conversations</a>
