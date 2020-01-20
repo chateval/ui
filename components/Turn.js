f94ef351e38433a433fdf0addc4960f5b6e5b3a6
Arun Kirubarajan <kiruba@cis.upenn.edu>
2020-01-20 12:03:10 -0500

    Add multi-turn styling to conversation card

--- components/Turn.js
+++ components/Turn.js
@@ -2,7 +2,7 @@ const Turn = (props) => (
   <div className="col-md-6 vmargin">
     <div className="card">
       <div className="card-body">
-        <h6 className="card-title mb-2 text-muted">{props.prompt.prompt_text}</h6>
+        <h6 className="card-title mb-2 text-muted" style={{whiteSpace: "pre-line"}}>{props.prompt.prompt_text}</h6>
         {props.responses.map((response, index) => <div key={index}> <a className="badge badge-light font-weight-normal"> {response.name} </a> {response.response.response_text} </div> )}
       </div>
     </div>
