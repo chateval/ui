5e731efe16bae06fc4e47322558e72bf6cde8df5
Zhang Chen <e0397123@u.nus.edu>
2021-04-24 21:55:17 +0800

    disable download and submit link

--- pages/dstc10.js
+++ pages/dstc10.js
@@ -14,8 +14,8 @@ class DSTC10 extends Component {
           <h1 className="mt-5 font-weight-bold">DSTC10: Dialogue System Technology Challenge 10</h1>
           <h2 className="mt-5 font-weight-bold">Automatic Evaluation and Moderation of Open-domain Dialogue Systems</h2>
             <br />
-            <p>Click <a href="https://my.chateval.org/dstc10_data/">here</a> to download DSTC10 data.</p>
-            <p>Click <a href="https://my.chateval.org/dstc10submit/">here</a> to submit.</p>
+            <p>Click <a href="#">here</a> to download DSTC10 data.</p>
+            <p>Click <a href="#">here</a> to submit.</p>
             <hr />
             <p>There are two subtasks in this track:</p>
 
