35a21ca4f3f5de3eb5aaecf003efe0da2ca428b1
Zhang Chen <e0397123@u.nus.edu>
2021-06-16 10:13:43 +0800

    update information of subtask 1 on dstc10 page

--- pages/dstc10.js
+++ pages/dstc10.js
@@ -32,15 +32,24 @@ class DSTC10 extends Component {
                 In this task, our goal is to seek effective automatic dialogue evaluation metrics that exhibit the above properties. These metrics can serve as a proxy to human evaluation for fast prototyping of open-domain chatbots. We have identified the following datasets to test the effectiveness of the proposed evaluation metrics: 
                 <br /> <br />
                 <ul>
-                  <li>DSTC6 human evaluation data (Hori et al., 2017)</li>
-                  <li>DSTC7 human evaluation data (Galley et al., 2019)</li>
-                  <li>Persona-Chatlog dataset (See et al., 2019)</li>
-                  <li>USR dataset (Mehri & Eskenazi, 2020)</li>
-                  <li>FED dataset (Mehri & Eskenazi, 2020)</li>
+                  <li>DSTC6-Eval (D6) (Hori et al., 2017)</li>
+                  <li>DSTC7-Eval (D7) (Galley et al., 2019)</li>
+                  <li>Persona-Chatlog (PC) (See et al., 2019)</li>
+                  <li>PersonaChat-USR (UP) (Mehri & Eskenazi, 2020)</li>
+                  <li>TopicalChat-USR (TP) (Mehri & Eskenazi, 2020)</li>
+                  <li>FED-Turn (FT) (Mehri & Eskenazi, 2020)</li>
+                  <li>FED-Conversation (FC) (Mehri & Eskenazi, 2020)</li>
+                  <li>DailyDialog-Eval (GD) (Gupta et al., 2019)</li>
+                  <li>DailyDialog-Eval (ZD) (Zhao et al., 2020)</li>
+                  <li>PersonaChat-Eval (ZP) (Zhao et al., 2020)</li>
+                  <li>DailyDialog-Eval (ED) (Huang et al., 2020)</li>
+                  <li>Empathetic-Eval (EE) (Huang et al., 2020)</li>
+                  <li>ConvAI2-Eval (EC)  (Huang et al., 2020)</li>
+                  <li>HUMOD (HU) (Merdivan et al., 2020)</li>
                 </ul>
-                During the development phase, the participants need to propose different evaluation metrics. participants can submit their metric scores via ChatEval. The Pearson and Spearman correlations between the submitted scores and corresponding human scores will be computed per evaluation category per dataset. The correlation results will be reported in the leaderboard on the evaluation category basis. The submissions will be ranked by the average correlation scores across all the categories of all the datasets.</p>
+                During the development phase, the participants need to propose different evaluation metrics. participants can submit their metric scores via ChatEval. The Spearman correlations (&pi;) between the submitted scores and corresponding human scores will be computed per evaluation category per dataset. The correlation results will be reported in the leaderboard on the evaluation category basis. The submissions will be ranked by the average correlation scores across all the categories of all the datasets.</p>
             <p style={{marginLeft: 60 + 'px'}} align='justify'>During the final evaluation phase, we will release a hidden evaluation set and all the submitted metrics will be evaluated with the hidden evaluation set. The final ranking will be based on the performance on both the development set and the hidden test set.</p>
-            <p style={{marginLeft: 60 + 'px'}} align='justify'>Note: The above datasets are only allowed for testing the proposed metrics, not for training the evaluation systems. The performance on the hidden test set has higher importance on the final ranking of the submissions.</p>
+            <p style={{marginLeft: 60 + 'px'}} align='justify'>Note: The above datasets are only allowed for validating the proposed metrics, not for training the evaluation systems. The performance on the hidden test set has higher importance on the final ranking of the submissions.</p>
             <hr />
             <ul>
               <li><b>Subtask 2: Moderation of Open-domain Dialogue Systems</b></li>
@@ -71,21 +80,20 @@ class DSTC10 extends Component {
                 <thead>
                     <tr>
                       <th scope="col">System</th>
-                      <th scope="col">D6 (&rho;)</th>
                       <th scope="col">D6 (&pi;)</th>
-                      <th scope="col">D7 (&rho;)</th>
                       <th scope="col">D7 (&pi;)</th>
-                      <th scope="col">PC (&rho;)</th>
                       <th scope="col">PC (&pi;)</th>
-                      <th scope="col">UP (&rho;)</th>
                       <th scope="col">UP (&pi;)</th>
-                      <th scope="col">UT (&rho;)</th>
                       <th scope="col">UT (&pi;)</th>
-                      <th scope="col">FT (&rho;)</th>
                       <th scope="col">FT (&pi;)</th>
-                      <th scope="col">FC (&rho;)</th>
                       <th scope="col">FC (&pi;)</th>
-                      <th scope="col">AVG (&rho;)</th>
+                      <th scope="col">ZD (&pi;)</th>
+                      <th scope="col">ZP (&pi;)</th>
+                      <th scope="col">GD (&pi;)</th>
+                      <th scope="col">ED (&pi;)</th>
+                      <th scope="col">EC (&pi;)</th>
+                      <th scope="col">EE (&pi;)</th>
+                      <th scope="col">HU (&pi;)</th>
                       <th scope="col">AVG (&pi;)</th>
                     </tr>
                 </thead>
@@ -93,21 +101,20 @@ class DSTC10 extends Component {
                   {evaluation.results.map(result => (
                       <tr key={result.id}>
                         <td key="td01">{result.sys}</td>
-                        <td key="td02">{result.d6r}</td>
                         <td key="td03">{result.d6s}</td>
-                        <td key="td04">{result.d7r}</td>
                         <td key="td05">{result.d7s}</td>
-                        <td key="td06">{result.pcr}</td>
                         <td key="td07">{result.pcs}</td>
-                        <td key="td08">{result.upr}</td>
                         <td key="td09">{result.ups}</td>
-                        <td key="td10">{result.utr}</td>
                         <td key="td11">{result.uts}</td>
-                        <td key="td12">{result.ftr}</td>
                         <td key="td13">{result.fts}</td>
-                        <td key="td14">{result.fcr}</td>
                         <td key="td15">{result.fcs}</td>
-                        <td key="td16">{result.avgr}</td>
+                        <td key="td15">{result.zds}</td>
+                        <td key="td15">{result.zps}</td>
+                        <td key="td15">{result.gds}</td>
+                        <td key="td15">{result.eds}</td>
+                        <td key="td15">{result.ecs}</td>
+                        <td key="td15">{result.ees}</td>
+                        <td key="td15">{result.hus}</td>
                         <td key="td17">{result.avgs}</td>
                       </tr>
                   ))}
@@ -146,6 +153,10 @@ class DSTC10 extends Component {
           <p>[5] Mehri, S., & Eskenazi, M. (2020). USR: An Unsupervised and Reference Free Evaluation Metric for Dialog Generation. arXiv preprint arXiv:2005.00456.</p>
           <p>[6] Mehri, S., & Eskenazi, M. (2020, July). Unsupervised Evaluation of Interactive Dialog with DialoGPT. In Proceedings of the 21th Annual Meeting of the Special Interest Group on Discourse and Dialogue (pp. 225-235).</p>
           <p>[7] Zhang C., D’Haro L.F., Banchs R.E., Friedrichs T., Li H. (2021) Deep AM-FM: Toolkit for Automatic Dialogue Evaluation. In Conversational Dialogue Systems for the Next Decade. Lecture Notes in Electrical Engineering, vol 704. Springer, Singapore.</p>
+          <p>[8] Zhao, T., Lala, D., & Kawahara, T. (2020, July). Designing Precise and Robust Dialogue Response Evaluators. In Proceedings of the 58th Annual Meeting of the Association for Computational Linguistics (pp. 26-33).</p>
+          <p>[9] Gupta, P., Mehri, S., Zhao, T., Pavel, A., Eskenazi, M., & Bigham, J. P. (2019, September). Investigating Evaluation of Open-Domain Dialogue Systems With Human Generated Multiple References. In Proceedings of the 20th Annual SIGdial Meeting on Discourse and Dialogue (pp. 379-391).</p>
+          <p>[10] Huang, L., Ye, Z., Qin, J., Lin, L., & Liang, X. (2020, November). GRADE: Automatic Graph-Enhanced Coherence Metric for Evaluating Open-Domain Dialogue Systems. In Proceedings of the 2020 Conference on Empirical Methods in Natural Language Processing (EMNLP) (pp. 9230-9240).</p>
+          <p>[11] Merdivan, E., Singh, D., Hanke, S., Kropf, J., Holzinger, A., & Geist, M. (2020). Human annotated dialogues dataset for natural conversational agents. Applied Sciences, 10(3), 762.</p>
           <p>&nbsp;</p>
 
         </main>
@@ -165,28 +176,27 @@ DSTC10.getInitialProps = async function() {
         id: 'automatic_evaluation',
         evalset: { 
           name: "Open-domain Dialogue Evaluation",
-          description: "The leaderboard showing names of submissions and their corresponding Pearson & Spearman Correlation for each evaluation dataset. (Explanation of abbreviations: D6 - DSTC6, D7 - DSTC7, PC - Persona-Chatlog, UP - USR-Persona, UT - USR-Topical, FT - FED-Turn, FC - FED-Conversation, AVG - Average, ρ - Pearson score, π - Spearman score)"
+          description: "The leaderboard showing names of submissions and their corresponding Pearson & Spearman Correlation for each evaluation dataset."
         },
         results: [
-         {
-            id: 'bleu_result',
-            sys: 'BLEU',
-            d6r: '-',
-            d6s: '-',
-            d7r: '-',
-            d7s: '-',
-            pcr: '-',
-            pcs: '-',
-            upr: '-',
-            ups: '-',
-            utr: '-',
-            uts: '-',
-            ftr: '-',
-            fts: '-',
-            fcr: '-',
-            fcs: '-',
-            avgr: '-',
-            avgs: '-'
+          {
+            id: 'amfm_result',
+            sys: 'Deep AM-FM',
+            d6s: '0.105',
+            d7s: '-0.033',
+            pcs: '0.082',
+            ups: '0.131',
+            uts: '0.269',
+            fts: '0.046',
+            fcs: '0.121',
+            zds: '0.198',
+            zps: '0.236',
+            gds: '-0.046',
+            eds: '0.164',
+            ecs: '0.094',
+            ees: '-0.027',
+            hus: '0.011',
+            avgs: '0.097'
           }
         ]
       }
