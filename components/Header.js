91ec809eec3adcbbd402211c8f16010b22adff6b
Arun Kirubarajan <kiruba@cis.upenn.edu>
2021-07-07 21:57:12 -0400

    Add google analytics script

--- components/Header.js
+++ components/Header.js
@@ -8,6 +8,14 @@ const Header = () => (
       <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossOrigin="anonymous"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossOrigin="anonymous"></script>
       <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossOrigin="anonymous"></script>
+
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-200432660-1"></script>
+      <script>
+        window.dataLayer = window.dataLayer || []
+        function gtag(){dataLayer.push(arguments)}
+        gtag('js', new Date())
+        gtag('config', 'UA-200432660-1')
+      </script>
     </head>
 
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
